a8f28f39de365ff238dc2e9ad7615fd657c19bba
Chris Gmyr <cmgmyr@gmail.com>
2015-04-04 09:44:01 -0400

    updated for $thread->latestMessage

--- resources/views/messenger/index.blade.php
+++ resources/views/messenger/index.blade.php
@@ -12,7 +12,7 @@
             <?php $class = $thread->isUnread($currentUserId) ? 'alert-info' : ''; ?>
             <div id="thread_list_{{$thread->id}}" class="media alert {!!$class!!}">
                 <h4 class="media-heading">{!! link_to('messages/' . $thread->id, $thread->subject) !!}</h4>
-                <p id="thread_list_{{$thread->id}}_text">{!! $thread->latestMessage()->body !!}</p>
+                <p id="thread_list_{{$thread->id}}_text">{!! $thread->latestMessage->body !!}</p>
                 <p><small><strong>Participants:</strong> {!! $thread->participantsString(Auth::id()) !!}</small></p>
             </div>
             @endforeach
